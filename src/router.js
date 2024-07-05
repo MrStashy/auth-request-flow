49a38ea81cddb997eb11bd8249922f1a596d22c3
Will Baxter <bax.will@googlemail.com>
2024-07-05 12:14:29 +0100

    Log in and verify login

--- src/router.js
+++ src/router.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-    const payload = { username: mockUser.username }
-    const token = jwt.sign(payload, process.env.JWT_SECRET)
-    res.status(200).json(token)
+router.post("/login", (req, res) => {
+  const payload = { username: mockUser.username };
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  console.log(token)
+  res.status(200).json(token);
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  const authKey = req.headers.bearer;
+  try {
+    jwt.verify(authKey, process.env.JWT_SECRET);
+    res.status(200).json({ profile: mockUser.profile });
+  } catch (e) {
+    res.status(401).json({ error: "Login failed" });
+  }
 });
-
 
 module.exports = router;
